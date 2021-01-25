1ee1372c901535cc282db5d65ae70a8d710bfbc0
babaiyu <bayuputra9a@gmail.com>
2021-01-25 21:39:01 +0700

    Minimalism code at tk3_3.c

--- tk3_3.c
+++ tk3_3.c
@@ -24,39 +24,39 @@ typedef struct DOSEN {
 } Dosen;
 
 int main() {
-  Dosen dataDosen[TOTAL_DATA];
+  Dosen dosen[TOTAL_DATA];
 
   for (int i = 0; i < TOTAL_DATA; i++) {
     int index = i + 1;
     printf("Masukkan Data Dosen ke %d \n", index);
-    printf("No Dosen: "); scanf("%d", &dataDosen[i].noDosen);
-    printf("NIDN: "); scanf("%d", &dataDosen[i].nidn);
-    printf("Nama: "); scanf("%s", dataDosen[i].nama);
+    printf("No Dosen: "); scanf("%d", &dosen[i].noDosen);
+    printf("NIDN: "); scanf("%d", &dosen[i].nidn);
+    printf("Nama: "); scanf("%s", dosen[i].nama);
 
     printf("\nMasukkan Alamat Dosen ke %d\n", index);
-    printf("Nama Jalan: "); scanf("%s", dataDosen[i].alamat.namaJalan);
-    printf("Nomor Rumah: "); scanf("%d", &dataDosen[i].alamat.nomorRumah);
-    printf("Kota: "); scanf("%s", dataDosen[i].alamat.kota);
-    printf("Provinsi: "); scanf("%s", dataDosen[i].alamat.provinsi);
+    printf("Nama Jalan: "); scanf("%s", dosen[i].alamat.namaJalan);
+    printf("Nomor Rumah: "); scanf("%d", &dosen[i].alamat.nomorRumah);
+    printf("Kota: "); scanf("%s", dosen[i].alamat.kota);
+    printf("Provinsi: "); scanf("%s", dosen[i].alamat.provinsi);
 
     printf("\nMasukkan Tanggal Lahir Dosen ke %d", index);
-    printf("Masukkan Tanggal: (ex: 09)"); scanf("%s", dataDosen[i].tanggalLahir.tanggal);
-    printf("Masukkan Bulan: (ex: 12)"); scanf("%s", dataDosen[i].tanggalLahir.bulan);
-    printf("Masukkan Tahun: (ex: 1999)"); scanf("%d", &dataDosen[i].tanggalLahir.tahun);
+    printf("Masukkan Tanggal: (ex: 09)"); scanf("%s", dosen[i].tanggalLahir.tanggal);
+    printf("Masukkan Bulan: (ex: 12)"); scanf("%s", dosen[i].tanggalLahir.bulan);
+    printf("Masukkan Tahun: (ex: 1999)"); scanf("%d", &dosen[i].tanggalLahir.tahun);
     printf("\n=================== Data ke %d selesai ===================\n", index);
   }
 
   for (int i = 0; i < TOTAL_DATA; i++) {
     int index = i + 1;
     printf("\n=================== Data ke %d ===================\n", index);
-    printf("No Dosen = %d \n", dataDosen[i].noDosen);
-    printf("NIDN = %d \n", dataDosen[i].nidn);
-    printf("Nama = %s \n", dataDosen[i].nama);
-    printf("Nama Jalan = %s \n", dataDosen[i].alamat.namaJalan);
-    printf("Nomor Rumah = %d \n", dataDosen[i].alamat.nomorRumah);
-    printf("Kota = %s \n", dataDosen[i].alamat.kota);
-    printf("Provinsi = %s \n", dataDosen[i].alamat.provinsi);
-    printf("Tanggal Lahir = %s/%s/%d \n", dataDosen[i].tanggalLahir.tanggal, dataDosen[i].tanggalLahir.bulan, dataDosen[i].tanggalLahir.tahun);
+    printf("No Dosen = %d \n", dosen[i].noDosen);
+    printf("NIDN = %d \n", dosen[i].nidn);
+    printf("Nama = %s \n", dosen[i].nama);
+    printf("Nama Jalan = %s \n", dosen[i].alamat.namaJalan);
+    printf("Nomor Rumah = %d \n", dosen[i].alamat.nomorRumah);
+    printf("Kota = %s \n", dosen[i].alamat.kota);
+    printf("Provinsi = %s \n", dosen[i].alamat.provinsi);
+    printf("Tanggal Lahir = %s/%s/%d \n", dosen[i].tanggalLahir.tanggal, dosen[i].tanggalLahir.bulan, dosen[i].tanggalLahir.tahun);
   }
   
   return 0;
